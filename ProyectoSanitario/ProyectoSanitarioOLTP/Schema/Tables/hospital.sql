95d06c52074199101e5b5624cb7b201ad270903d
DavidGuzmanZ <ndguzmanz@soe.uagrm.edu.bo>
2025-02-25 16:19:41 -0800

    Add_Schema_tables_
    Agregamos esquema 'san', ya tablas 'hospital' y 'paciente'

---
+++ ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/hospital.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [san].[hospital]
+(
+	[cod_hospital] INT NOT NULL, 
+    [nombre] NVARCHAR(50) NULL, 
+    [ciudad] NVARCHAR(50) NULL, 
+    [telefono] NVARCHAR(50) NULL, 
+    CONSTRAINT [PK_hospital_cod] PRIMARY KEY ([cod_hospital]) 
+)
---
+++ ProyectoSanitario/ProyectoSanitarioOLTP/Schema/Tables/paciente.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [san].[paciente]
+(
+	[dni] INT NOT NULL , 
+    [nombre_apellido] NVARCHAR(50) NULL, 
+    [fecha_nacimiento] DATE NULL, 
+    [numero_seguridad_social] NVARCHAR(50) NULL, 
+    [otros_datos] NVARCHAR(MAX) NULL, 
+    CONSTRAINT [PK_paciente_dni] PRIMARY KEY ([dni])
+)
